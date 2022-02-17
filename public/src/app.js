35964d13b087ac2baf0ce005e7bf7a41a02fea4b
nahashonkariuki <N.kariuki@alustudent.com>
2022-02-17 19:01:33 +0200

    first commit

--- public/src/app.js
+++ public/src/app.js
@@ -1,7 +1,7 @@
 const header = document.querySelector(".header")
 let counter = 0
 function changecolor(){
-    let colors = "green", "orange", "pink", "purple"
+    let colors = "green", blue
     header.getElementsByClassName.colors = colors[counter]
     counter = (counter + 1) % colors.length
 }
